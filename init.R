2611d84cd945f2b332224f30a720d495ce40beb8
SkiMsyk <Sakai.Masayuki@keio.jp>
2022-12-14 11:33:10 +0900

    first commit: init gitbook

---
+++ init.R
@@ -0,0 +1,20 @@
+require(tidyverse)
+require(bookdown)
+
+# create book directory
+if(! "book" %in% list.dirs()){
+  dir.create("book")
+}
+
+# create gitbook 
+if(! "index.Rmd" %in% list.files("book")){
+  bookdown::create_gitbook("book")
+  file.remove(c("book/book.Rproj", 
+                "book/README.md", 
+                "book/02-cross-refs.Rmd",
+                "book/03-parts.Rmd",
+                "book/04-citations",
+                "book/05-blocks.Rmd",
+                "book/06-share.Rmd",
+                "book/07-references.Rmd"))
+}
